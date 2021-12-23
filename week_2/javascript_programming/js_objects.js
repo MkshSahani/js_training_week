62175342ca6816f8154524580ffa7ec76b203b37
MkshSahani <msco18333@gmail.com>
2021-12-23 13:21:38 +0530

    javascript and angular added

--- week_2/javascript_programming/js_objects.js
+++ week_2/javascript_programming/js_objects.js
@@ -1,8 +1,9 @@
 // object in java script. 
 
 var binary_tree_node = {
-    "data" : 12, 
-    "next" : null, 
+    data : 12, 
+    left : null, 
+    right : null,  
     print_data : show_data
 }
 
@@ -10,6 +11,20 @@ function show_data() {
     console.log(this.data); 
 }
 
-console.log(binary_tree_node.data, binary_tree_node.next);
-
+console.log(binary_tree_node.data, binary_tree_node.left, binary_tree_node.right);
 binary_tree_node.print_data();
+
+// object creater 
+function binary_tree_node_object(data, left_child, right_child) {
+    this.data = data; 
+    this.left_child = left_child; 
+    this.right_child = right_child; 
+    this.show = () => {
+        console.log(this.data, this.left_child, this.right_child); 
+        return true;
+    }
+}
+// binary_tree_node_object 
+binary_tree_node = new binary_tree_node_object(12, null, null)
+var data = binary_tree_node.show();
+console.log(data); 
